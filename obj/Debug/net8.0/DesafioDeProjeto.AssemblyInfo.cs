ed7f079503e415a6a4c53fe6ec457e2c7faab2dd
Diego Brunari <diego.skp@hotmail.com>
2024-09-28 23:41:11 -0300

    add readme

--- obj/Debug/net8.0/DesafioDeProjeto.AssemblyInfo.cs
+++ obj/Debug/net8.0/DesafioDeProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioDeProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e93969f6b4016a096ded2d5dc33c7c2677dba2b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioDeProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioDeProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
